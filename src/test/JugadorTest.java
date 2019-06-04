6a101d43c023d3635b91bd9000f4640c9fc3435f
Jenko Budnechky <jbudnechky@fi.uba.ar>
2019-06-04 17:00:26 -0300

    Agregado de clases Jugador y JugadorTest (sin terminar). Y Diagrama UML muy basico (en astahUML).

---
+++ src/test/JugadorTest.java
@@ -0,0 +1,26 @@
+package test;
+
+import org.junit.Test;
+
+public class JugadorTest {
+
+    /*Es la entidad controlable por el usuario, el cual puede moverse
+    por el mapa, y conseguir materiales que haya en su camino.
+    Al iniciar la partida, el jugador inicia con un hacha de madera
+    en el inventario.
+    El jugador debe poder desplazarse en todas las direcciones y puede moverse
+    por todos los casilleros vacíos (es decir, sin ningún material).
+    */
+    @Test
+    public void test01JugadorSeInicializaConUnItemEnElInventario(){
+        Jugador jugador = Jugador nuevoJugadorConHachaMadera();
+        assertEquals(jugador.getInventario().length(), 1);
+    }
+    @Test
+    public void test02JugadorSeInicializaConUnHachaDeMadera(){
+        Jugador jugador = Jugador nuevoJugadorConHachaMadera();
+        Herramienta herramientaJugador = jugador.getHerramientaActual();
+        assertTrue(herramientaJugador instanceof Hacha);
+        assertTrue(true/* Comparacion con doubledispatch para el tipo de Hacha*/);
+    }
+}
