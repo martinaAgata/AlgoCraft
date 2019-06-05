99f0afe0bf5fc3f3717b8fdb572b826970688861
Belen Aguada <mbelenaguada@gmail.com>
2019-06-05 00:35:22 -0300

    se modifico el jugador

--- src/main/Jugador.java
+++ src/main/Jugador.java
@@ -1,33 +1,23 @@
+package main;
 
+import main.estrategias.DesgasteLineal;
+import main.herramientas.HachaMadera;
+import main.herramientas.Herramienta;
+import main.materiales.Material;
 
-public class Jugador(){
+import java.util.ArrayList;
+import java.util.List;
+
+public class Jugador {
     //Atributos
-    private ArrayList<Usable> inventario;
+    private List<Material> inventario;
     private Herramienta herramientaActual;
 
     //Constructor
-    private Jugador(){
-        this.inventario = new ArrayList<Usable>();
+    public Jugador(){
+        this.inventario = new ArrayList<>();
+        HachaMadera hachaMadera = new HachaMadera(10, 10, new DesgasteLineal());
+        this.herramientaActual = hachaMadera;
     }
-
-    public static Jugador nuevoJugadorConHachaMadera(){
-        Jugador jugadorConHachaMadera = new Jugador();
-        Hacha hachaMadera = Hacha nuevaHachaMaddera();
-        jugadorConHachaMadera.agregarAInventario(hachaMadera);
-        jugadorConHachaMadera.herramientaActual = hachaMadera;
-        return jugadorConHachaMadera;
-    }
-
-    public void desgastarMaterial(Material material){
-            this.herramientaActual.usar(material);
-        }
-
-    private void agregarAInventario(Usable usable){
-            this.inventario.add(usable);
-        }
-    private void getHerramientaActual(){
-        //Completar funcion QUITAR
-        //No hay ninguna especificacion de como se utilizara/elegira una Herramienta. QUITAR
-        return (this.inventario[0]);
-        }
+    
 }
