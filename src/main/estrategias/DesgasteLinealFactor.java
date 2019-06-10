0c5cfed6195142280d0d20a8af0fc4fd2ffb35f2
Jenko Budnechky <jbudnechky@fi.uba.ar>
2019-06-10 14:27:59 -0300

    Modificación de DesgasteLinealFactor como un desgastador lineal genérico

--- src/main/estrategias/DesgasteLinealFactor.java
+++ src/main/estrategias/DesgasteLinealFactor.java
@@ -3,9 +3,15 @@ package main.estrategias;
 import main.Estado;
 
 public class DesgasteLinealFactor implements EstrategiaDesgaste {
+    private double factorDesgaste;
+
+    public DesgasteLinealFactor(double factorDesgaste){
+        this.factorDesgaste = factorDesgaste;
+    }
+
     @Override
     public Estado desgastar(int fuerza, Estado estado) {
-        double fuerzaFinal = ((double) fuerza) / 1.5;
+        double fuerzaFinal = ((double) fuerza) * this.factorDesgaste;
         return estado.desgastar((int)fuerzaFinal);
     }
 }
