c8675c769576d3ca9b5cb1379137aa241dfad9cb
martinaagata <martinapanetta@hotmail.com>
2019-06-11 01:36:23 -0300

    Modifico clase Juego y Mapa

--- src/main/mapa/Mapa.java
+++ src/main/mapa/Mapa.java
@@ -1,26 +1,22 @@
 package main.mapa;
-import main.Jugador;
 import main.Ubicable;
-import main.materiales.Material;
-
 import java.util.ArrayList;
-import java.util.Random;
 
 public class Mapa {
      private ArrayList<ArrayList<Casillero>> casilleros;
      private final int filas;
      private final int columnas;
-     private final int x; // veremos estos nombres xd
+     private final int x;
      private final int y;
 
-     private Mapa() {
+     public Mapa() {
          casilleros = new ArrayList<>();
          this.filas = 20;
          this.columnas = 20;
          this.inicializarMapa();
      }
 
-     private void inicializarMapa() {
+     public void inicializarMapa() {
          for (int i=0; i<=this.filas; i++) {
              this.casilleros[i] = new ArrayList<>();
              for (int j=0; j<=this.columnas; j++) {
@@ -29,7 +25,7 @@ public class Mapa {
          }
      }
 
-    private void ubicarEnCasillero(Ubicable ubicable) {
+    public void ubicarEnCasillero(Ubicable ubicable) {
         x = (int)(Math.random()*filas);
         y = (int)(Math.random()*columnas);
         casilleros[x][y].guardarUbicable(ubicable);
