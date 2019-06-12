2b124a11ef93845b72c51e4e77bfd5aca849bf49
martinaagata <martinapanetta@hotmail.com>
2019-06-12 12:24:06 -0300

    Corrijo tests de hacha madera por problema de merge

--- test/main/HachaMaderaTests.java
+++ test/main/HachaMaderaTests.java
@@ -16,41 +16,27 @@ import static org.junit.Assert.assertThat;
 public class HachaMaderaTests {
 
     @Test
-    public void test01HachaMaderaSeUsaContraMaderaReduceSuDurabilidad(){
-<<<<<<< HEAD
-
+    public void test01HachaMaderaSeUsaContraMaderaReduceSuDurabilidad() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Madera madera = new Madera();
         hachaMadera.usar(madera);
         assertThat(hachaMadera.getDurabilidad(), is(98));
     }
 
     @Test
-    public void test05HachaMaderaSeUsaContraPiedraReduceSuDurabilidad(){
-<<<<<<< HEAD
+    public void test05HachaMaderaSeUsaContraPiedraReduceSuDurabilidad() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Piedra piedra = new Piedra();
         hachaMadera.usar(piedra);
-
         assertThat(hachaMadera.getDurabilidad(), is(98));
    }
 
     @Test
-    public void test06HachaMaderaSeUsaContraMetalReduceSuDurabilidad(){
-<<<<<<< HEAD
+    public void test06HachaMaderaSeUsaContraMetalReduceSuDurabilidad() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Metal metal = new Metal();
         hachaMadera.usar(metal);
         assertThat(hachaMadera.getDurabilidad(), is(98));
@@ -58,25 +44,17 @@ public class HachaMaderaTests {
 
     @Test
     public void test04HachaMaderaSeUsaContraDiamanteReduceSuDurabilidad() {
-<<<<<<< HEAD
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Diamante diamante = new Diamante();
         hachaMadera.usar(diamante);
         assertThat(hachaMadera.getDurabilidad(), is(98));
     }
 
     @Test
-    public void test05HachaMaderaSeUsaContraMaderaReduceSuDurabilidadDeADos(){
-<<<<<<< HEAD
+    public void test05HachaMaderaSeUsaContraMaderaReduceSuDurabilidadDeADos() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Madera madera = new Madera();
         Integer durabilidadHachaMadera = hachaMadera.getDurabilidad();
         hachaMadera.usar(madera);
@@ -88,13 +66,9 @@ public class HachaMaderaTests {
     }
 
     @Test
-    public void test06HachaMaderaSeUsaContraPiedraReduceSuDurabilidadDeADos(){
-<<<<<<< HEAD
+    public void test06HachaMaderaSeUsaContraPiedraReduceSuDurabilidadDeADos() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Piedra piedra = new Piedra();
         Integer durabilidadHachaMadera = hachaMadera.getDurabilidad();
         hachaMadera.usar(piedra);
@@ -106,13 +80,9 @@ public class HachaMaderaTests {
     }
 
     @Test
-    public void test07HachaMaderaSeUsaContraMetalReduceSuDurabilidadDeADos(){
-<<<<<<< HEAD
+    public void test07HachaMaderaSeUsaContraMetalReduceSuDurabilidadDeADos() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Metal metal = new Metal();
         Integer durabilidadHachaMadera = hachaMadera.getDurabilidad();
         hachaMadera.usar(metal);
@@ -124,13 +94,9 @@ public class HachaMaderaTests {
     }
 
     @Test
-    public void test08HachaMaderaSeUsaContraDiamanteReduceSuDurabilidadDeADos(){
-<<<<<<< HEAD
+    public void test08HachaMaderaSeUsaContraDiamanteReduceSuDurabilidadDeADos() {
         ConstructorHacha constructor = new ConstructorHacha();
         Hacha hachaMadera = constructor.construirHachaMadera();
-=======
-        Hacha hachaMadera = ConstructorHacha.construirHachaMadera();
->>>>>>> a3d961f5c7e117f60d6f0107976ebe64a88fb5a7
         Diamante diamante = new Diamante();
         Integer durabilidadHachaMadera = hachaMadera.getDurabilidad();
         hachaMadera.usar(diamante);
