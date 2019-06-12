bd07dce9c4dbff3cc47853ef2c363190d9b1a670
Belen Aguada <mbelenaguada@gmail.com>
2019-06-12 01:07:13 -0300

    se modifico detector paatron

---
+++ src/main/herramientas/DetectorPatron.java
@@ -0,0 +1,43 @@
+package main.herramientas;
+
+import main.mapa.Mapa;
+
+import java.util.Optional;
+
+public abstract class DetectorPatron {
+
+        private Optional<DetectorPatron> siguiente = Optional.empty();
+
+        public DetectorPatron() {
+
+        }
+
+        public DetectorPatron(DetectorPatron siguiente) {
+            this.siguiente = Optional.of(siguiente);
+        }
+
+        public Optional<Herramienta> resolver(Mapa mapita) {
+            if (puedoResolver(mapita)) {
+                return Optional.of(crearHerramienta());
+            } else if (siguiente.isPresent()) {
+                return siguiente.get().resolver(mapita);
+            } else {
+                return Optional.empty();
+            }
+        }
+
+        private boolean puedoResolver(Mapa mapa) {
+            // El equals del mapa tiene que fijarse que ambos tengan mismo ancho y alto
+            // Luego que casillero a casillero esten ocupados o desocupados de misma forma
+            // si estan ocupados, ver que sea con lo mismo (equals -> hay que redefinir el equals
+            // en material y que solo chequee que sean del mismo material)
+            return mapa.equals(this.getMapaPatron());
+        }
+
+        protected abstract Herramienta crearHerramienta();
+
+        protected abstract Mapa getMapaPatron();
+
+    }
+}
+
