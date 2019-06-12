20a4d4f6b2c7921991620776f2addac2dccacc0c
Belen Aguada <mbelenaguada@gmail.com>
2019-06-12 00:32:11 -0300

    borre que se cree el hacha desde la clase Hacha

--- src/main/herramientas/Hacha.java
+++ src/main/herramientas/Hacha.java
@@ -12,41 +12,11 @@ import java.util.Optional;
 
 public class Hacha extends Herramienta {
 
-    /*----- Hacha Madera -----*/
-    private static final int DURABILIDAD_HACHA_MADERA = 100;
-    private static final int FUERZA_HACHA_MADERA = 2;
-    private static final double FACTOR_DESGASTE_MADERA = 1;
-    /*----- Hacha Piedra -----*/
-    private static final int DURABILIDAD_HACHA_PIEDRA = 200;
-    private static final int FUERZA_HACHA_PIEDRA = 5;
-    private static final double FACTOR_DESGASTE_PIEDRA = 1;
-    /*----- Hacha Metal -----*/
-    private static final int DURABILIDAD_HACHA_METAL = 400;
-    private static final int FUERZA_HACHA_METAL = 10;
-    private static final double FACTOR_DESGASTE_METAL = 0.5;
-
     public Hacha(EstrategiaDesgaste estrategia, int durabilidad, int fuerza) {
         this.estrategia = estrategia;
         this.estado = new EstadoVivo(durabilidad);
         this.fuerza = fuerza;
     }
-
-    public static Hacha nuevaHachaMadera() {
-        return (new Hacha(new DesgasteLinealFactor(FACTOR_DESGASTE_MADERA), DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA));
-    }
-
-    public static Hacha nuevaHachaPiedra() {
-        return (new Hacha(new DesgasteLinealFactor(FACTOR_DESGASTE_PIEDRA), DURABILIDAD_HACHA_PIEDRA, FUERZA_HACHA_PIEDRA));
-    }
-
-    public static Hacha nuevaHachaMetal() {
-        return (new Hacha(new DesgasteLinealFactor(FACTOR_DESGASTE_METAL), DURABILIDAD_HACHA_METAL, FUERZA_HACHA_METAL));
-    }
-
-    /*@Override
-    protected void desgastarMaterial(Material material) {
-        material.desgastarCon(this);
-    }*/
 
     @Override
     public Optional<Desgastable> desgastarContra(Desgastable desgastable){
