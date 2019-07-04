833497d34b44954732efb0baa33d21b794731464
jenkobud <jbudnechky@fi.uba.ar>
2019-07-04 14:58:38 -0300

    tests

--- test/modelo/herramientas/HachaMaderaTests.java
+++ test/modelo/herramientas/HachaMaderaTests.java
@@ -9,9 +9,11 @@ import modelo.materiales.Metal;
 import modelo.materiales.Piedra;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertFalse;
 import static modelo.juego.ConstantesJuego.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 
 public class HachaMaderaTests {
@@ -163,5 +165,64 @@ public class HachaMaderaTests {
             hachaMadera.usar(piedra);
         }
 
+    }
+
+    @Test
+    public void testHachaMaderaEsIgualASiMisma(){
+        ConstructorHacha constructor = new ConstructorHacha();
+        constructor
+                .conMaterial(new Madera())
+                .conDurabilidad(DURABILIDAD_HACHA_MADERA)
+                .conDesgaste(DESGASTE_HACHA_MADERA)
+                .conFuerza(FUERZA_HACHA_MADERA);
+        Hacha hachaMadera = constructor.construir();
+        assertTrue(hachaMadera.esIgualA(hachaMadera));
+    }
+
+    @Test
+    public void testNuevaHachaMaderaEsIgualANuevaHachaMadera(){
+        Hacha hachaMadera = new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        Hacha nuevaHachaMadera =  new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        assertTrue(hachaMadera.esIgualA(nuevaHachaMadera));
+    }
+
+    @Test
+    public void testHachaMaderaUsadaNOEsIgualANuevaHachaMadera(){
+        Hacha hachaMadera = new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        hachaMadera.desgastarContra(new Madera());
+        Hacha nuevaHachaMadera =  new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        assertTrue(hachaMadera.esIgualA(nuevaHachaMadera));
+    }
+
+    @Test
+    public void testHachaMaderaNOEsIgualAHachaPiedra(){
+        Hacha hachaMadera = new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        Hacha hachaPiedra = new Hacha(DESGASTE_HACHA_PIEDRA, DURABILIDAD_HACHA_PIEDRA, FUERZA_HACHA_PIEDRA, new Piedra());
+        assertFalse(hachaMadera.esIgualA(hachaPiedra));
+    }
+
+    @Test
+    public void testHachaMaderaNOEsIgualAHachaMetal(){
+        Hacha hachaMadera = new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        Hacha hachaMetal = new Hacha(DESGASTE_HACHA_METAL, DURABILIDAD_HACHA_METAL, FUERZA_HACHA_METAL, new Metal());
+        assertFalse(hachaMadera.esIgualA(hachaMetal));
+    }
+
+    @Test
+    public void testHachaMaderaNOEsIgualAPico(){
+        Hacha hachaMadera = new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        Pico pico = new Pico(DESGASTE_PICO_MADERA, DURABILIDAD_PICO_MADERA, FUERZA_PICO_MADERA, new Madera());
+        assertFalse(hachaMadera.esIgualA(pico));
+        pico = new Pico(DESGASTE_PICO_PIEDRA, DURABILIDAD_PICO_PIEDRA, FUERZA_PICO_PIEDRA, new Piedra());
+        assertFalse(hachaMadera.esIgualA(pico));
+        pico = new Pico(DESGASTE_PICO_METAL, DURABILIDAD_PICO_METAL, FUERZA_PICO_METAL, new Metal());
+        assertFalse(hachaMadera.esIgualA(pico));
+    }
+
+    @Test
+    public void testHachaMaderaNOEsIgualAPicoFino(){
+        Hacha hachaMadera = new Hacha(DESGASTE_HACHA_MADERA, DURABILIDAD_HACHA_MADERA, FUERZA_HACHA_MADERA, new Madera());
+        PicoFino picoFino = new PicoFino(DESGASTE_PICO_FINO, DURABILIDAD_PICO_FINO, FUERZA_PICO_FINO);
+        assertFalse(hachaMadera.esIgualA(picoFino));
     }
 }
